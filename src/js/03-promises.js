9bc02d9a9e671c15a742e5e07743acc540840e7b
Swarp <m.kleban@terawatt-group.com>
2023-03-04 23:36:57 +0200

    Added third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,55 @@
+// all modules
+import Notiflix from 'notiflix';
+
+// one by one
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Змінні
+formEl = document.querySelector('.form');
+let firstDelay = null;
+let stepDelay = null;
+let amount = null;
+let currentDelay = null;
+
+// Прослуховування submit та створення Promise
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  firstDelay = Number(formEl.elements.delay.value);
+  stepDelay = Number(formEl.elements.step.value);
+  amount = formEl.elements.amount.value;
+  for (let i = 1; i <= amount; i += 1) {
+    if (i === 1) {
+      currentDelay = firstDelay;
+      checkPromise();
+    } else {
+      currentDelay += stepDelay;
+      checkPromise();
+    }
+
+    // Функція для перевірки результату Promise
+    function checkPromise() {
+      createPromise(i, currentDelay)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  }
+});
+
+// Функція створення Promise
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  console.log(shouldResolve);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
